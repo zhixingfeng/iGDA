68fef99686c0cfa68937ca62e7b5680db3fd1882
zhixingfeng <zxfeng.thu@gmail.com>
2016-08-05 15:57:59 -0400

    Initial Commit

---
+++ iGDA/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  iGDA
+//
+//  Created by Zhixing Feng on 8/5/16.
+//  Copyright (c) 2016 Zhixing Feng. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
