992b56b2fa14625e19abc31e5930b75c35000a92
zhixingfeng <zxfeng.thu@gmail.com>
2016-09-26 13:55:53 -0400

    update freqsetminer

---
+++ src/modules/freqsetminer/freqsetminer.cpp
@@ -0,0 +1,9 @@
+//
+//  freqsetminer.cpp
+//  iGDA
+//
+//  Created by Zhixing Feng on 9/26/16.
+//  Copyright (c) 2016 Zhixing Feng. All rights reserved.
+//
+
+#include "freqsetminer.h"
---
+++ src/modules/freqsetminer/freqsetminer.h
@@ -0,0 +1,14 @@
+//
+//  freqsetminer.h
+//  iGDA
+//
+//  Created by Zhixing Feng on 9/26/16.
+//  Copyright (c) 2016 Zhixing Feng. All rights reserved.
+//
+
+#ifndef __iGDA__freqsetminer__
+#define __iGDA__freqsetminer__
+
+#include <stdio.h>
+
+#endif /* defined(__iGDA__freqsetminer__) */
