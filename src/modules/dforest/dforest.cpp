04347e4bf38008fdc6378fae4a5374294db0e239
Zhixing Feng <zhixingfeng@ZhixingdeMacBook-Pro.local>
2016-12-01 13:15:36 -0500

    dforest

---
+++ src/modules/dforest/dforest.cpp
@@ -0,0 +1,9 @@
+//
+//  bumphunter.cpp
+//  iGDA
+//
+//  Created by Zhixing Feng on 16/12/1.
+//  Copyright © 2016年 Zhixing Feng. All rights reserved.
+//
+
+#include "bumphunter.hpp"
---
+++ src/modules/dforest/dforest.h
@@ -0,0 +1,14 @@
+//
+//  bumphunter.hpp
+//  iGDA
+//
+//  Created by Zhixing Feng on 16/12/1.
+//  Copyright © 2016年 Zhixing Feng. All rights reserved.
+//
+
+#ifndef bumphunter_hpp
+#define bumphunter_hpp
+
+#include <stdio.h>
+
+#endif /* bumphunter_hpp */
