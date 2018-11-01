9632e3483972ad8209e82718b772b7155d96f5fc
zhixingfeng <zxfeng.thu@gmail.com>
2018-11-01 11:54:52 -0400

    rm graphviz

--- tools/graphviz/graphviz-2.40.1/tclpkg/pkgIndex.tcl
+++
@@ -1,5 +0,0 @@
-package ifneeded Tcldot 2.40.1 "
-	load [file join $dir libtcldot.0.dylib] Tcldot"
-package ifneeded Tclpathplan 2.40.1 "
-	load [file join $dir libtclplan.0.dylib] Tclpathplan"
-# end
