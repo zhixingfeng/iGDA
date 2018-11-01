feda7c25c381027db847930884a4b9270d8830ca
zhixingfeng <zxfeng.thu@gmail.com>
2018-11-01 11:36:09 -0400

    add graphviz to tools

---
+++ tools/graphviz/graphviz-2.40.1/tclpkg/tcldot/pkgIndex.tcl
@@ -0,0 +1,2 @@
+package ifneeded Tcldot 2.40.1 "
+	load [file join $dir libtcldot.0.dylib] Tcldot"
---
+++ tools/graphviz/graphviz-2.40.1/tclpkg/tclpathplan/pkgIndex.tcl
@@ -0,0 +1,2 @@
+package ifneeded Tclpathplan 2.40.1 "
+	load [file join $dir libtclplan.0.dylib] Tclpathplan"
