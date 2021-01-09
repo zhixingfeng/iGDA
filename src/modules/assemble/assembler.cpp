11bf4efd665668f098aa26fd7ba62ad35629358a
zhixingfeng <zxfeng.thu@gmail.com>
2021-01-08 21:36:43 -0500

    use rl_ann_clust[j].start > rl_ann_clust[i].end  instead of rl_ann_clust[j].start > = rl_ann_clust[i].end in assemble::ann_to_graph()

--- src/modules/assemble/assembler.cpp
+++ src/modules/assemble/assembler.cpp
@@ -1296,7 +1296,7 @@ void Assembler::ann_to_graph(Graph &gp, string ann_file, double min_prop, double
         for (int64_t j = 0; j < this->rl_ann_clust.size(); ++j){
             if (j == i) continue;
 
-            if (rl_ann_clust[j].start < rl_ann_clust[i].start || rl_ann_clust[j].start >= rl_ann_clust[i].end || rl_ann_clust[j].end < rl_ann_clust[i].end)
+            if (rl_ann_clust[j].start < rl_ann_clust[i].start || rl_ann_clust[j].start > rl_ann_clust[i].end || rl_ann_clust[j].end < rl_ann_clust[i].end)
                 continue;
             
             if (rl_ann_clust[j].start == rl_ann_clust[i].start && rl_ann_clust[j].end == rl_ann_clust[i].end && j > i)
